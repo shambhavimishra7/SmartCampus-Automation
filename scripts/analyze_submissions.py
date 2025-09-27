f64cc0d562514bcb55a9a5b2955d134f6855ddf5
shambhavi <141444999+shambhavimishra7@users.noreply.github.com>
2025-09-27 15:03:24 +0530

    Create analyze_submissions.py

---
+++ scripts/analyze_submissions.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('../data/attendance_updated.csv')
+
+# Homework submission rate
+submission_rate = data['HomeworkSubmitted'].mean() * 100
+print(f"Homework Submission Rate: {submission_rate:.2f}%")
+
+# Plot homework submission status
+data['HomeworkSubmitted'].value_counts().plot(kind='bar', title='Homework Submission Status')
+plt.savefig('../visuals/report_sample.png')
+plt.show()
